586dbb9e21b6c4af42d6a7820d48d0233346e3f6
河儿 <60065901+jay-pro@users.noreply.github.com>
2021-09-08 21:37:50 +0700

    update components

--- myapp/src/components/Categories.js
+++ myapp/src/components/Categories.js
@@ -1,7 +1,23 @@
-import React from "react";
+import styled from "styled-components";
+import { categories } from "../data";
+import { mobile } from "../responsive";
+import CategoryItem from "./CategoryItem";
 
-function Categories() {
-  return <div></div>;
-}
+const Container = styled.div`
+  display: flex;
+  padding: 20px;
+  justify-content: space-between;
+  ${mobile({ padding: "0px", flexDirection: "column" })}
+`;
+
+const Categories = () => {
+  return (
+    <Container>
+      {categories.map((item) => (
+        <CategoryItem item={item} key={item.id} />
+      ))}
+    </Container>
+  );
+};
 
 export default Categories;
